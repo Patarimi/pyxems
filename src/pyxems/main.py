9aa986578fe5cda0c67bc683c2207b7dc0f6b9dc
Patarimi <mpqqch@gmail.com>
2026-02-07 13:51:27 +0100

    rewrite everything

--- src/pyxems/main.py
+++ src/pyxems/main.py
@@ -1,51 +1,106 @@
-# reference:https://github.com/thliebig/openEMS/blob/557483aa21d93fb053825272c5a7eb58f5016af5/openems.cpp#L821
-
 from pathlib import Path
-import xml.etree.ElementTree as ET
+from typing import Literal
+from dataclasses import dataclass, field
+
+Boundary = Literal["MUR", "PEC", "PMC", "PML"]
 
 
-def openEMS_config() -> ET.Element:
-    oems_config = ET.Element("openEMS")
-    return oems_config
+@dataclass(frozen=True)
+class Material:
+    name: str
+    epsilon_r: float
+    mu_r: float
+    kappa: float = 0.0
+    sigma: float = 0.0
+
+    def to_xml(self) -> str:
+        return f'<{self.name} Epsilon="{self.epsilon_r:g}" Mue="{self.mu_r:g}" Kappa="{self.kappa:g}" Sigma="{self.sigma:g}" />'
 
 
-def add_FDTD(oems_config: ET.Element) -> ET.Element:
-    FDTD = ET.SubElement(oems_config, "FDTD")
-    return FDTD
+@dataclass(frozen=True)
+class BoundaryCond:
+    xmin: Boundary = "MUR"
+    xmax: Boundary = "MUR"
+    ymin: Boundary = "MUR"
+    ymax: Boundary = "MUR"
+    zmin: Boundary = "MUR"
+    zmax: Boundary = "MUR"
+
+    def to_xml(self) -> str:
+        elem = "<BoundaryCond "
+        for dim in ["xmin", "xmax", "ymin", "ymax", "zmin", "zmax"]:
+            value = getattr(self, dim)
+            elem += f'{dim}="{value}" '
+        elem += "/>"
+        return elem
 
 
-def add_boundary(
-    oems_config: ET.Element,
-) -> ET.Element:
-    FDTD = ET.SubElement(oems_config, "FDTD")
-    BondaryCond = ET.SubElement(
-        FDTD,
-        "BoundaryCond",
-        attrib={
-            "xmin": "MUR",
-            "xmax": "MUR",
-            "ymin": "MUR",
-            "ymax": "MUR",
-            "zmin": "MUR",
-            "zmax": "MUR",
-        },
+@dataclass(frozen=True)
+class FDTDConfig:
+    max_time_step: int = 1_000_000
+    boundary_cond: BoundaryCond = field(default_factory=BoundaryCond)
+    exitation: int = 0
+
+    def to_xml(self) -> str:
+        xml = f'<FDTD MaxTimeStep="{self.max_time_step}">\n'
+        xml += f"    {self.boundary_cond.to_xml()}\n"
+        xml += f"    <Excitation Type={self.exitation} />\n"
+        xml += "</FDTD>\n"
+        return xml
+
+
+@dataclass(frozen=True)
+class Line:
+    axe: Literal["X", "Y", "Z"]
+    position: list[float] = field(default_factory=list)
+
+    def to_xml(self) -> str:
+        xml = f'<{self.axe.upper()}Lines Qty="{len(self.position)}">'
+        xml += ",".join([f"{value}" for value in self.position])
+        xml += f"</{self.axe.upper()}Lines>"
+        return xml
+
+
+@dataclass(frozen=True)
+class ContinousStructure:
+    coordinates_system: int = 0
+    lines: dict[str, Line] = field(default_factory=dict)
+    background_material: Material = field(
+        default_factory=lambda: Material("BackgroundMaterial", 1.0, 1.0)
     )
-    return BondaryCond
+
+    def __post_init__(self):
+        for axe in ["x", "y", "z"]:
+            self.lines[axe] = Line(axe.upper())
+
+    def add_line(self, axe: Literal["x", "y", "z"], position: float):
+        self.lines[axe].position.append(position)
+
+    def to_xml(self) -> str:
+        xml = f'<ContinuousStructure CoordSystem="{self.coordinates_system}">\n'
+        xml += '    <RectilinearGrid DeltaUnit="0.001" CoordSystem="0">\n'
+        for line in self.lines.values():
+            xml += f"        {line.to_xml()}\n"
+        xml += "    </RectilinearGrid>\n"
+        xml += f"    {self.background_material.to_xml()}\n"
+        xml += "</ContinuousStructure>\n"
+        return xml
 
 
-def add_CSX(oems_config: ET.Element) -> ET.Element:
-    CSX = ET.SubElement(oems_config, "CSX")
-    return CSX
+@dataclass(frozen=True)
+class PyXEMSConfig:
+    fdtd: FDTDConfig = field(default_factory=FDTDConfig)
+    csx: ContinousStructure = field(default_factory=ContinousStructure)
+
+    def to_xml(self) -> str:
+        xml = "<openEMS>\n"
+        xml += self.fdtd.to_xml()
+        xml += self.csx.to_xml()
+        xml += "</openEMS>"
+        return xml
 
 
-def write_openEMS_xml(filename: Path | str, config: ET.Element):
-    tree = ET.ElementTree(config)
-    tree.write(filename, encoding="utf-8", xml_declaration=True)
-
-
-if __name__ == "__main__":
-    oems_config = openEMS_config()
-    add_boundary(oems_config)
-    add_FDTD(oems_config)
-    add_CSX(oems_config)
-    write_openEMS_xml("openEMS_config.xml", oems_config)
+def write_openEMS_xml(filename: Path | str, config: FDTDConfig):
+    with open(filename, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>\n')
+        f.write(config.to_xml())
