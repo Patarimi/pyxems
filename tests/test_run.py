7c4fa45858dde2d9d9c956530b3e78a349fc86c6
Patarimi <mpqqch@gmail.com>
2026-02-18 14:17:19 +0100

    fix find_openems and add logging

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,9 +1,11 @@
 from pathlib import Path
-from pyxems.run import simulate, check_config
+from pyxems.run import simulate, check_config, find_openems_executable
 import pytest
 
 
-@pytest.mark.skipif(not check_config(), reason="OpenEMS exe not found.")
+@pytest.mark.skipif(
+    not check_config(), reason=f"OpenEMS exe not found. {find_openems_executable()}"
+)
 def test_simulate(tmp_path: Path):
     config_path = Path(__file__).parent / "data" / "simp_patch.xml"
     result = simulate(config_path, tmp_path)
